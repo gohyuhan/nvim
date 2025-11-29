99dff64b14b6ffcae4f325e8694fdc3e655c6790
yuhan~夕飯 ¯\_(ツ)_/¯ <gohyuhan123456@gmail.com>
2025-11-30 01:20:18 +0800

    update

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,43 @@
 local options = {
   formatters_by_ft = {
+    -- Lua
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+
+    -- Python
+    python = { "black" },
+
+    -- Go
+    go = { "gofumpt" },
+
+    -- Rust
+    rust = { "rustfmt" },
+
+    -- Web Stack (Prettier handles all of these)
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    javascriptreact = { "prettier" }, -- This handles .jsx
+    typescriptreact = { "prettier" }, -- This handles .tsx
+    svelte = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    json = { "prettier" },
+    yaml = { "prettier" },
+    markdown = { "prettier" },
+    ["markdown.inline"] = { "prettier" },
+
+    -- Shell / Bash
+    sh = { "shfmt" },
+    bash = { "shfmt" },
+
+    -- Dockerfile & Vim
+    -- We do NOT list them here explicitly. 
+    -- Because we set `lsp_fallback = true` in your keymap, 
+    -- these will automatically use the Language Server (dockerls / vimls) 
+    -- to format without needing extra config.
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    timeout_ms = 250,
+    lsp_fallback = true,
+  },
 }
-
-return options
