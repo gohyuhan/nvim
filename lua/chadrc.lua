7d6058383529cd3e23c98062ae0a0af9ca52009a
yuhan~夕飯 ¯\_(ツ)_/¯ <gohyuhan123456@gmail.com>
2025-11-30 15:04:00 +0800

    update

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,16 +1,18 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@class ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "yoru",
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  theme = "catppuccin",
+  transparency = true,
+  -- Add this block to fix comment visibility under transparency
+  hl_override = {
+    Comment = { fg = "#9399b2", italic = true },
+    ["@comment"] = { fg = "#9399b2", italic = true },
+  },
 }
 
 -- M.nvdash = { load_on_startup = true }
